0cb17b61545e7dd3f217cc1f6b9bdf0330fc925e
hvdx23 <hv25@students.waikato.ac.nz>
2024-05-01 02:45:44 +1200

    Colour scheme made. Y axis cannot be hidden. Trying to show the name of the y axis to variable names

--- viz.py
+++ viz.py
@@ -3,31 +3,39 @@ import matplotlib.pyplot as plt
 import mplcursors
 import numpy as np
 import matplotlib.colors as mcolors
+from matplotlib.colors import TwoSlopeNorm
 
 df = pd.read_csv('cleaned_review.csv')
 
 df['Playtime_Score'] = df['Playtime_Score'] + np.random.normal(0, 10, size=len(df))
 df['Score'] = df['Score'] + np.random.normal(0, 1, size=len(df))
 
-colors = ["blue", "gray", "red"]
 
-cmap = mcolors.LinearSegmentedColormap.from_list("bgr", colors)
+colors = ["red", "yellow", "green"]
 
-scatter = plt.scatter(df['Playtime'], df['Playtime_Score'], c=df['Playtime_Score'], cmap=cmap)
 
+cmap = mcolors.LinearSegmentedColormap.from_list("ryg", colors)
+
+vmin = df['Playtime_Score'].min()
+vmax = df['Playtime_Score'].max()
+divnorm = TwoSlopeNorm(vmin=vmin, vcenter=0, vmax=vmax)
+
+
+scatter = plt.scatter(df['Playtime'], df['Playtime_Score'], c=df['Playtime_Score'], cmap=cmap, norm=divnorm)
 
 plt.xlabel('Playtime')
-plt.ylabel('Playtime * Score')
-plt.title('Scatterplot of Playtime vs Playtime * Score')
+plt.ylabel('Imperession')
+plt.title('Scatterplot of Playtime vs Impression')
+
 
 sm = plt.cm.ScalarMappable(cmap=scatter.cmap, norm=scatter.norm)
-sm.set_array([])
+sm.set_array([])  # Set empty array to avoid data range issues
+
 
 cursor = mplcursors.cursor(scatter)
 
 @cursor.connect("add")
 def on_add(sel):
   sel.annotation.set_text(df['Review'].iloc[sel.target.index])
-
 
 plt.show()
