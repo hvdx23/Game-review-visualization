df45e0cfc1154703cae9330a7c2371ff3a73a240
hvdx23 <hv25@students.waikato.ac.nz>
2024-05-01 01:20:07 +1200

    Interactivity added. Woking on further review of playtime vs review data to scatter the plot further

--- viz.py
+++ viz.py
@@ -1,16 +1,20 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
+import mplcursors
 
 df = pd.read_csv('review.csv')
 
-
-plt.scatter(df['Playtime'], df['Score'], c=df['Score'])
-
+scatter = plt.scatter(df['Playtime'], df['Score'], c=df['Score'])
 
 plt.xlabel('Playtime')
 plt.ylabel('Score')
 plt.title('Scatterplot of Playtime vs Score')
 
+# Use mplcursors to add interactivity
+cursor = mplcursors.cursor(scatter)
+
+@cursor.connect("add")
+def on_add(sel):
+    sel.annotation.set_text(df['Review'].iloc[sel.target.index])
 
 plt.show()
